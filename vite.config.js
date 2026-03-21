bef72005982e102fe0307435e0fdd89ed1e61fd0
ElLorans <lorenzo.cerreta@gmail.com>
2026-03-22 00:34:59 +0100

    remove config

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig(({ mode }) => ({
-  base: process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/',
-  plugins: [react()],
-}))
