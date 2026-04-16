844731bb92e28cad66f0347dd91e5fe9c3966c0a
timendum <timedum@gmail.com>
2026-04-16 15:23:54 +0200

    Format file

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -35,7 +35,9 @@ export default class ErrorBoundary extends Component<Props, State> {
         <div className="screen" style={{ textAlign: "center", padding: "4rem 1rem" }}>
           <h1>Something went wrong</h1>
           <p>An unexpected error occurred. Try reloading the page or reset the game.</p>
-          <div style={{ display: "flex", gap: "1rem", justifyContent: "center", marginTop: "1rem" }}>
+          <div
+            style={{ display: "flex", gap: "1rem", justifyContent: "center", marginTop: "1rem" }}
+          >
             <button onClick={() => window.location.reload()}>Reload</button>
             <button onClick={this.handleReset}>Reset Game</button>
           </div>
